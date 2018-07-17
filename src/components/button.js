706c1f978e213ce0708f1de4664b8eb6375b1172
London Walker <nodnolreklaw@gmail.com>
2018-07-17 08:57:35 -0600

    built and styled clock component

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Button = (title) => {
-    return {
+    return (
         <button class="button">
             {title}
         </button>
-    }
+    )
 }
 
 export default Button;
