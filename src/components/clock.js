c2a92493cb5a2e7ce8123ed13fc38c1ce8d292d8
London Walker <nodnolreklaw@gmail.com>
2018-07-17 09:14:01 -0600

    styled clock with grid and flex and stuff

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,21 +4,21 @@ class Clock extends component {
     render() {
         return (
             <div className="clock">
-                <div className="clock__days">
+                <div className="clock__days clock__box">
                     <label className="clock__title">DAYS</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">300</label>
                 </div>
-                <div className="clock__hours">
+                <div className="clock__hours clock__box">
                     <label className="clock__title">HOURS</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">16</label>
                     </div>
-                <div className="clock__minutes">
+                <div className="clock__minutes clock__box">
                     <label className="clock__title">MINUTES</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">42</label>
                 </div>                    
-                <div className="clock__seconds">
+                <div className="clock__seconds clock__box">
                     <label className="clock__title">SECONDS</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">22</label>
                 </div>
             </div>
         )
